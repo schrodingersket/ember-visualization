06943fce71a2f25934c3986d3816bce280c10f4d
Jonathan Thompson <jonathan@urbaneinnovation.com>
2015-03-11 16:45:29 -0600

    EV-1: Added SVG container that resizes with the window.

---
+++ addon/components/svg-container.js
@@ -0,0 +1,56 @@
+import Ember from 'ember';
+
+/* global d3 */
+
+export default Ember.Component.extend({
+
+  /**
+   * The type of element to render this view into. By default,
+   * samples will render as `<div/>` elements.
+   */
+  tagName: 'div',
+
+  /**
+   * Class names associated with this view.
+   */
+  classNames: ['svg-container'],
+
+  /**
+   * Holds a reference to this
+   */
+  svg: null,
+
+  /**
+   * Adds and stores the base svg element for this view
+   *
+   * Be sure to call this._super() when extending this method!
+   */
+  didInsertElement: function() {
+    this.set('svg', d3.selectAll(this.$()).append('svg:svg'));
+
+    this.get('svg')
+        .attr('width', this.$().width())
+        .attr('height', this.$().height());
+
+    Ember.$(window).on('resize', this.resizeHandler.bind(this));
+  },
+
+  /**
+   * Called when the component is to be removed from the DOM.
+   *
+   * This is where we unbind our resize handler.
+   */
+  willDestroyElement: function() {
+    Ember.$(window).off('resize', this.resizeHandler);
+  },
+
+  /**
+   * Handles window resize events to re-render SVG. This method is what provides
+   * us with a reponsive SVG window.
+   */
+  resizeHandler: function() {
+    this.get('svg')
+      .attr('width', this.get('element').width())
+      .attr('height', this.get('element').height());
+  }
+});
---
+++ tests/unit/components/svg-container-test.js
@@ -0,0 +1,31 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+moduleForComponent('svg-container', {
+  // specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+});
+
+test('it renders with svg element', function(assert) {
+  assert.expect(7);
+
+  // Creates the component instance
+  //
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  //
+  var element = this.render();
+  assert.equal(component._state, 'inDOM');
+  assert.equal(component.get('element').tagName, 'div'.toUpperCase(), 'matches `div`');
+  assert.equal(component.get('classNames').contains('svg-container'), true, 'contains `svg-container` class');
+  assert.ok(component.get('svg'));
+
+  // Verify SVG dimensions are equal to the container's dimensions
+  //
+  assert.equal(element.find('svg').width(), element.width());
+  assert.equal(element.find('svg').height(), element.height() - 4);
+});
